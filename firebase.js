18bea9a719635c3cf6fd419ba154ec181c3d2023
LAMFARRAD ELARBI <127060237+Arbilamfarr@users.noreply.github.com>
2024-12-28 15:36:17 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,15 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCjajOvMJ4InIoBbtytPlwsgHoSBqjRg3c",
-  authDomain: "consomationwatter.firebaseapp.com",
-  projectId: "consomationwatter",
-  storageBucket: "consomationwatter.appspot.com",
-  messagingSenderId: "518874808145",
-  appId: "1:518874808145:web:d69045f3e810c0de76dd70",
-  measurementId: "G-NXN1WMZ7BH",
-};
+// your configuration de firebase 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
